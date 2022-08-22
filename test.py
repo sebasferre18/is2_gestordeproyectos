6354bdc0bf599ff339f41a2f9df2914af98f6816
sebasferre18 <sebas.ferre18@gmail.com>
2022-08-22 09:19:05 -0400

    Prueba de documentacion

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
+"""Prueba de documentacion"""
+
 def func(x):
     return x + 1
 
 
 def test_answer():
+    """Funcion de prueba.
+
+    Retorna True si func(a) es igual a 5"""
     assert func(3) == 5
 
 
 class TestClass(object):
+    """Prueba de documentacion"""
     def test_one(self):
         x = "this"
         assert 'h' in x

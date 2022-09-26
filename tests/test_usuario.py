ed53ebc43e52233ff16579d2c4c84d946eea6fa9
sebasferre18 <sebas.ferre18@gmail.com>
2022-09-25 23:38:13 -0400

    Agregado cuatro funciones de prueba para la app de Usuarios

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,3 +1,45 @@
 import pytest
 from usuarios.models import Usuario
+from django.test import Client
+
+
+class TestVistas:
+
+    def test_listar_usuario(self, django_user_model):
+        username = "usuario1"
+        password = "pass"
+        user = django_user_model.objects.create_user(username=username, password=password)
+        client = Client()
+        client.force_login(user)
+        respuesta = client.get('/usuarios/listar_usuarios/')
+        assert respuesta.status_code == 200
+
+    def test_nuevo_usuario(self, django_user_model):
+        username = "usuario1"
+        password = "pass"
+        user = django_user_model.objects.create_user(username=username, password=password)
+        client = Client()
+        client.force_login(user)
+        respuesta = client.get('/usuarios/nuevo_usuario/')
+        assert respuesta.status_code == 200
+
+    @pytest.mark.django_db
+    def test_modificar_usuario(self, django_user_model):
+        username = "usuario1"
+        password = "pass"
+        user = django_user_model.objects.create_user(username=username, password=password)
+        usuario = Usuario(user=user)
+        client = Client()
+        client.force_login(user)
+        respuesta = client.get('/1/modificar_usuario/')
+        assert respuesta.status_code == 200
+
+
+class TestModelos:
+
+    def test_usuario(self, django_user_model):
+        username = "usuario1"
+        password = "pass"
+        user = django_user_model.objects.create_user(username=username, password=password)
+        usuario = Usuario(user=user, ci=1234567, telefono="0444 444444", fecha_nac="01/01/1998")
 

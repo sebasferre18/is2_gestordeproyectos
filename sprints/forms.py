f7962e7a5448bf486218cf161377def2afde314b
SSWalrus <mj_155@hotmail.com>
2022-09-29 22:45:59 -0400

    rama de sprints

---
+++ sprints/forms.py
@@ -0,0 +1,45 @@
+from .models import Rol, Permiso
+from django import forms
+
+"""
+Definicion de los formularios para la gestion de roles.
+"""
+
+
+class RolForm(forms.ModelForm):
+    """Formulario generico con los campos del modelo Rol"""
+    class Meta:
+        model = Rol
+        fields = [
+            'nombre',
+            'descripcion',
+            'permiso'
+        ]
+        labels = {
+            'nombre': 'Nombre',
+            'descripcion': 'Descripcion',
+            'permiso': 'Permisos',
+        }
+        widgets = {
+
+            'permiso': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RolForm, self).__init__(*args, **kwargs)
+        self.fields['permiso'].queryset = Permiso.objects.all().exclude(es_admin=True)
+
+
+class PermisoForm(forms.ModelForm):
+    """Formulario generico con los campos del modelo Permiso"""
+    class Meta:
+        model = Permiso
+        fields = [
+            'nombre',
+            'es_admin'
+        ]
+        labels = {
+            'nombre': 'Nombre',
+            'es_admin': 'Es permiso administrativo',
+        }
+
